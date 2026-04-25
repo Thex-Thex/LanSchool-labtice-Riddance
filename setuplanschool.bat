29c0edbe3c268070dedbbd9552f0f1d79002e92d
Thex <dario.chapuis@divtec.ch>
2026-04-25 17:43:18 +0200

    adding support for labtice
    adding support for labtice other app same than lanschool

--- setuplanschool.bat
+++ setuplanschool.bat
@@ -1,6 +1,5 @@
 @echo off
 
- copy .\n.bat %temp%
  call :isAdmin
 
  if %errorlevel% == 0 (
@@ -22,9 +21,11 @@
 	copy %temp%\n.bat C:\Windows\System32
 	del %temp%\n.bat
 	sc config LSAirClientService start= demand
+   sc config "Service Labtice 0.1" start= demand
  exit /b
 
  :UACPrompt
+   copy .\n.bat %temp%
    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
